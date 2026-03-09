982fc70744eee755d15e61d124e28ea93f662f61
Indhu1725 <mattaindira998@gmail.com>
2026-03-09 21:27:55 +0530

    Added ConstructorOverloading program

---
+++ OneDrive/Desktop/Indhu/ConstructorOverloading/ConstructorOverloading/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace ConstructorOverloading
+{
+    class Student
+    {
+        int id;
+        string name;
+        public Student()//Default Constructor
+        {
+            id = 0;
+            name = "Unknown";
+        }
+        public Student(int i, string n) // Parameterized Constructor
+        {
+            id = i;
+            name = n;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Id: " + id + " Name: " + name);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();              // Default constructor
+            Student s2 = new Student(152, "Indira"); // Parameterized constructor
+
+            s1.Display();
+            s2.Display();
+        }
+    }
+}
