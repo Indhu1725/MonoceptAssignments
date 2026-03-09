4af1e73b198692ecd57132d03916f1da1da65c70
Indhu1725 <mattaindira998@gmail.com>
2026-03-09 21:16:05 +0530

    Added BoxingUnboxing program

---
+++ OneDrive/Desktop/Indhu/BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BoxingUnboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 50;
+            object obj = number;        //Boxing
+            int value = (int)obj;    // Unboxing
+
+            Console.WriteLine("Original value: " + number);
+            Console.WriteLine("After Boxing: " + obj);
+            Console.WriteLine("After Unboxing: " + value);
+        }
+    }
+}
