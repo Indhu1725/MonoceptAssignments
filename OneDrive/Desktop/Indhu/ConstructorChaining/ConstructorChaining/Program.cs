26d34934f45ad33018afbea47acd40199a696c2d
Indhu1725 <mattaindira998@gmail.com>
2026-03-09 21:33:12 +0530

    Added ConstructorChaining program

---
+++ OneDrive/Desktop/Indhu/ConstructorChaining/ConstructorChaining/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Employee
+{
+    int id;
+    string name;
+    public Employee()
+    {
+        Console.WriteLine("Default Constructor");
+    }
+
+    public Employee(int id) : this()
+    {
+        this.id = id;
+        Console.WriteLine("Constructor with Id: " + id);
+    }
+    public Employee(int id, string name) : this(id)
+    {
+        this.name = name;
+        Console.WriteLine("Constructor with Id and Name: " + id + " " + name);
+    }
+    static void Main()
+    {
+        Employee e = new Employee(152, "Indira");
+    }
+}
