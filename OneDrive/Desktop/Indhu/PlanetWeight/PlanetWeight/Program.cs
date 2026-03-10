4bfda60421d21973505aea86ccef09b400bfacc2
Indhu1725 <mattaindira998@gmail.com>
2026-03-10 19:29:56 +0530

    Added PlanetWeight program

---
+++ OneDrive/Desktop/Indhu/PlanetWeight/PlanetWeight/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+namespace PlanetWeight
+{
+    enum Planet
+    {
+        MERCURY,
+        VENUS,
+        EARTH,
+        MARS,
+        JUPITER,
+        SATURN,
+        URANUS,
+        NEPTUNE
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<Planet, double> gravity = new Dictionary<Planet, double>()
+            {
+                { Planet.MERCURY, 0.38 },
+                { Planet.VENUS, 0.91 },
+                { Planet.EARTH, 1.00 },
+                { Planet.MARS, 0.38 },
+                { Planet.JUPITER, 2.34 },
+                { Planet.SATURN, 1.06 },
+                { Planet.URANUS, 0.92 },
+                { Planet.NEPTUNE, 1.19 }
+            };
+
+            Console.Write("Enter your weight on Earth: ");
+            double earthWeight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nWeight on different planets:");
+            foreach (Planet p in Enum.GetValues(typeof(Planet)))
+            {
+                double planetWeight = earthWeight * gravity[p];
+                Console.WriteLine($"{p}: {planetWeight:F2} kg");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
