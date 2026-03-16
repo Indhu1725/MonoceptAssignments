f443c5582b219b9332beae48028ee361b03a0aef
Indhu1725 <mattaindira998@gmail.com>
2026-03-16 18:10:35 +0530

    Added PigGame

---
+++ OneDrive/Desktop/Indhu/Console_Shape/PigGame/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace PigGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            int turn = 0;
+            Random r = new Random();
+
+            while (total < 20)
+            {
+                int turnScore = 0;
+                turn++;
+                Console.WriteLine("Turn: " + turn);
+
+                while (true)
+                {
+                    Console.Write("Roll or Hold (r/h): ");
+                    string choice = Console.ReadLine();
+
+                    if (choice == "r")
+                    {
+                        int dice = r.Next(1, 7);
+                        Console.WriteLine("Dice: " + dice);
+
+                        if (dice == 1)
+                        {
+                            Console.WriteLine("You rolled 1. Turn over.");
+                            turnScore = 0;
+                            break;
+                        }
+                        else
+                        {
+                            turnScore += dice;
+                            Console.WriteLine("Turn Score: " + turnScore);
+                        }
+                    }
+                    else if (choice == "h")
+                    {
+                        total += turnScore;
+                        Console.WriteLine("Total Score: " + total);
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("You reached 20 points in " + turn + " turns.");
+        }
+    }
+}
+                
